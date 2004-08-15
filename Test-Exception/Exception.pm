29b5a6c652de18795d2e5674dc3e85c1d2a3a908
adrianh <adrianh@d34b534f-cdcc-0310-9fd0-f1c7c63e638c>
2004-08-15 19:05:11 +0000

    Moving to Module::Build

--- Test-Exception/Exception.pm
+++ Test-Exception/Exception.pm
@@ -9,7 +9,7 @@ use base qw(Exporter);
 
 use vars qw($VERSION @EXPORT @EXPORT_OK);
 
-$VERSION = '0.18';
+$VERSION = '0.19';
 @EXPORT = qw(dies_ok lives_ok throws_ok lives_and);
 
 my $Tester = Test::Builder->new;
